9e68a87a55ca732c4110d76da1e30e3b108c84b8
Taehyeony <teahyun0540@gmail.com>
2024-12-20 04:16:58 +0900

    ArrayList add 구현

---
+++ src/list/arraylist/implement/ArrayList.java
@@ -0,0 +1,34 @@
+package list.arraylist.implement;
+
+public class ArrayList {
+    private final int size = 100;
+    private int index = 0;
+    private Object[] elements = new Object[size];
+
+    public boolean add(Object element) {
+        if(!isEmpty()) return false;
+        elements[index++] = element;
+        return true;
+    }
+
+    public boolean isEmpty() {
+        return index < size;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+
+        // 배열의 각 요소를 출력, 요소 사이에 ", "를 추가
+        for (int i = 0; i < index; i++) {
+            sb.append(elements[i]);
+            if (i < index - 1) {
+                sb.append(", ");
+            }
+        }
+
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ src/list/arraylist/implement/Main.java
@@ -0,0 +1,14 @@
+package list.arraylist.implement;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+
+        System.out.println("add(데이터)");
+        System.out.println(list);
+    }
+}
